7624f41309b778bd9d6e8c3368c2b1a8f79f05c2
Chris Whiting <mpro34@gmail.com>
2020-02-07 22:36:37 -0600

    Add basic LL implementation

--- data-structures/LinkedList.cpp
+++ data-structures/LinkedList.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-/**
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
- 
+
+struct Node {
+  int val;
+  Node* next;
+  Node(int x) : val(x), next(nullptr) {}
+};
+
 class LinkedList {
 /** Should support the following 
+* AppendNode (add node to end)
 * InsertNode
 * DeleteNode
 * Reverse
@@ -16,15 +16,59 @@ class LinkedList {
 * Detect a Loop
 **/
 private:
+  Node* root;
 
 public:
-  LinkedList() {};
+  LinkedList() { root = new Node(0); };
+  
+  LinkedList(int x) { root = new Node(x); };
+
+  void AppendNode(Node* node) { root->next = node; }
+
+  void InsertNode(Node* node, int index) 
+  {
+    // Insert a node at the specified index
+
+  }
+
+  void Delete(int index) 
+  {
+    // Delete the node at the specified index
+  
+  }
+
+  void Reverse() 
+  {
+    // Reverse the Linked List!
+
+  }
+
+  int Search(Node* node)
+  {
+    // return the index of the Node or -1 of not found
+
+    return -1;
+  }
+
+  bool LoopDetected()
+  {
+    // Whether a loop is detected or not.
+    
+    return false;
+  }
 
 };
 
 
 
-int main() {
+int main() 
+{
+  
+    LinkedList* mylist = new LinkedList(5);
+    Node* newnode = new Node(3);
+    mylist->AppendNode(newnode);
+
+    
     std::cout << "LinkedList" << std::endl;
     return 0;
 }
